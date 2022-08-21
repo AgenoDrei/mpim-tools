35f0849a6109439acfb41cf83b4ee477f82391e4
AgenoDrei <s.mueller1995@gmail.com>
2022-08-21 18:15:35 +0200

    Create boilerplate code for compability comparision

--- mpim_tools/create_matches.py
+++ mpim_tools/create_matches.py
@@ -3,26 +3,29 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 from mpim_tools.startup import names as n
-from mpim_tools.utils import cosine_similarity_words
-
+from mpim_tools.utils import cosine_similarity_words, get_mbti_type, compare_types
 
 cm = n['match']
 
 
 def create_matches(df, output_path, maximum_matches, mode='Relationship'):
     for i, row in tqdm(df.iterrows(), total=len(df)):
+        # Create table with matches for the current person
         matches_df = pd.DataFrame(columns=df.columns)
         matches_df['fitness'] = -1
+        matches_df['compatibility'] = -1
         gender = row[cm['GENDER_COL']]
-
         orientation = None
+
         if mode == 'Relationship' or mode == 'FWB':
             orientation = row[cm['ORIENTATION_COL']]
             if orientation in cm['DEF_HOMO']:
                 orientation = cm['HOMO']
-            if orientation == cm['NOT_KNOWN']:
+            if orientation != cm['HOMO'] and orientation != cm['HETERO'] and orientation != cm['BI']:
+                # TODO: Match non-binary folks with each other or other strategy
                 continue
 
+        # Compare the current person with everyone else
         for j, match_row in df.iterrows():
             match_gender = match_row[cm['GENDER_COL']]
             if row[n["notification"]["FORM_ID"]] == match_row[n["notification"]["FORM_ID"]]:
@@ -40,13 +43,27 @@ def create_matches(df, output_path, maximum_matches, mode='Relationship'):
                 if match_orientation == cm['NOT_KNOWN']:
                     continue
 
+            # Calculate match similarity based on questions
             match_fitness = calc_match_fitness(row, match_row, mode)
+
+            # Measure similarity by personality traits
+            match_compatibility = compare_personalities(row, match_row, mode)
+
             # match_row_dict = match_row.to_dict()
             match_row['fitness'] = match_fitness
             matches_df = matches_df.append(match_row, ignore_index=True)
 
-        matches_sorted_df = matches_df.sort_values(by="fitness", ascending=False)
-        matches_sorted_df.head(maximum_matches).to_csv(os.path.join(output_path, f'{row[n["notification"]["FORM_ID"]]}.csv'), index=False, sep=';')
+        # Get mix of similar, compatible and random matches for this person
+        top_matches = matches_df.sort_values(by="fitness", ascending=False)
+        top_matches = top_matches.head(maximum_matches - n["NUM_RANDOM_MATCHES"] - n["NUM_COMPATIBLE_MATCHES"]).copy()
+        compatible_matches = matches_df.sort_values(by="compatibility", ascending=False).head(n["NUM_COMPATIBLE_MATCHES"]).copy()
+        random_matches = matches_df.sample(n=n["NUM_RANDOM_MATCHES"])
+
+        top_matches = top_matches.append(random_matches, ignore_index=True)
+        top_matches = top_matches.append(compatible_matches, ignore_index=True)
+
+        # Save matches for this person
+        top_matches.to_csv(os.path.join(output_path, f'{row[n["notification"]["FORM_ID"]]}.csv'), index=False, sep=';')
 
 
 def calc_mcq_fitness(answer_a, answer_b):
@@ -69,7 +86,7 @@ def calc_match_fitness(person_a, person_b, mode):
         trust_comp = int(trust_comp) if not np.isnan(trust_comp) else 0
         weight_comp = n['VALUE_IMPORTANCE_MED'] - np.absolute(person_a[cm['range']['WEIGHT_COL']] - person_b[cm['range']['WEIGHT_COL']])
         weight_comp = int(weight_comp) if not np.isnan(weight_comp) else 0
-        fitness += weight_comp
+        fitness += weight_comp + trust_comp
 
     # MCQ categories
     for k, v in cm['mcq'].items():
@@ -88,3 +105,12 @@ def calc_match_fitness(person_a, person_b, mode):
     # hobby_comp = calc_mcq_fitness(person_a[n['match']['HOBBY_COL']], person_b[n['match']['HOBBY_COL']])
     # fitness = age_comp + faculty_comp + hobby_comp + trait_comp + ll_comp + belief_comp + prio_comp + marriage_comp + children_comp + trust_comp
     return fitness
+
+
+def compare_personalities(person_a, person_b, mode):
+    personality_type_a = get_mbti_type(person_a)
+    personality_type_b = get_mbti_type(person_b)
+
+    compatible = compare_types(personality_type_a, personality_type_b)
+
+    return compatible
--- mpim_tools/utils.py
+++ mpim_tools/utils.py
@@ -24,3 +24,26 @@ def cosine_similarity_words(words1, words2):
     magB = math.sqrt(sum(c2.get(k, 0)**2 for k in terms))
     res = dotprod / (magA * magB)
     return res
+
+
+# TODO: Complete other subtypes
+def get_mbti_type(person):
+    version = 0
+    if names['match']['personality']['INTRO_EXTROVERSION'][0] == "With other people":
+        version += 1
+    if names['match']['personality']['INTRO_EXTROVERSION'][1] == "Enjoy":
+        version += 1
+    if names['match']['personality']['INTRO_EXTROVERSION'][2] == "Out-going and social":
+        version += 1
+    version = 'E' if version > 1 else 'I'
+
+    sensing = 0
+
+    feeling = 0
+    judging = 0
+
+    return version, 'S', 'F', 'J'
+
+
+def compare_types(personality_type_a, personality_type_b):
+    return 1
